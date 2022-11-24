a870ceab85660fc243af21c0ec6c051205c4319b
Ana Paula De Medeiros <90284746+Medeiros001@users.noreply.github.com>
2022-11-24 01:16:56 +0000

    Fizemos a consulta a banco, ficou bem legal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Importar nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 poyatos = Pessoa(1, "Henrique Poyatos")
@@ -7,3 +9,28 @@ print(poyatos)
 
 #Quero mostrar só o nome
 print(poyatos.nome)
+
+print("Daqui pra frente é o acesso ao banco...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto 
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+
+#Olha que simples
+novo = pessoaDAO.save(novo)
+
+#Consulta ao banco de novo
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
